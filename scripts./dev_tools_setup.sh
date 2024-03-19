f4b3d4f11dae3345af792aa0b1fcfd8f19d3297a
patelgr <byword-haddock.0j@icloud.com>
2024-03-19 19:12:51 -0400

    Create dev_tools_setup.sh

---
+++ scripts./dev_tools_setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Install Zsh
+sudo apt install zsh -y
+
+# Set Zsh as the default shell
+chsh -s $(which zsh)
+
+# Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Install Zsh plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Clean up
+sudo apt autoremove -y && sudo apt autoclean -y
